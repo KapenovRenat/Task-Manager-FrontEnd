03a63a9b84fd2b64a3831bd237ff6600bf037a89
Renat <rkapenov@ronasit.com>
2019-10-29 15:05:34 +0600

    #9 menu if not Active account hide menu items

--- src/public/components/menu/components/menu-items/index.tsx
+++ src/public/components/menu/components/menu-items/index.tsx
@@ -2,8 +2,9 @@ import { navigate } from '@reach/router';
 import { Icon } from 'antd';
 import { useState } from 'react';
 import *as React from 'react';
+import { connect } from 'react-redux';
 
-const MenuItems = () => {
+const MenuItems = ({user}: any) => {
     const [page, setPage] = useState<number>(1);
 
     const activePage = (pageSet: number) => {
@@ -19,11 +20,17 @@ const MenuItems = () => {
         <div className='menu-items'>
             <ul>
                 <li className={activePage(1)} onClick={()=>changePage(1, '/')}><Icon type="dashboard" />Main</li>
-                <li className={activePage(2)} onClick={()=>changePage(2, '/projects')}><Icon type="file-protect" />Projects</li>
-                <li className={activePage(3)} onClick={()=>changePage(3, '/users')}><Icon type="user" />Users</li>
+                {user.isActivate && <li className={activePage(2)} onClick={()=>changePage(2, '/projects')}><Icon type="file-protect" />Projects</li>}
+                {user.isActivate &&<li className={activePage(3)} onClick={()=>changePage(3, '/users')}><Icon type="user" />Users</li>}
+                {!user.isActivate &&<li>Activate your account!</li>}
             </ul>
         </div>
     );
 }
 
-export default MenuItems;
+export default connect(
+    (state: any) => ({
+        user: state.user
+    }),
+    dispatch => ({})
+)(MenuItems);
