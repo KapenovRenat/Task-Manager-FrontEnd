7c0b1d66deda8cd801b2224604a8b47bd264ce7f
Renat <rkapenov@ronasit.com>
2019-11-11 03:39:42 +0600

    configuration project

---
+++ src/pages/project/components/messager/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.scss';
+
+const MessageContainer = () => {
+    return (
+        <div className='messager'>
+            messager
+        </div>
+    );
+}
+
+export default MessageContainer;
--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,3 +1,4 @@
+import MessageContainer from '@pages/project/components/messager';
 import { Spin } from 'antd';
 import { useEffect } from 'react';
 import * as React from 'react';
@@ -12,7 +13,6 @@ const ProjectPage = ({ path, getProjectData, id, project, isLoading }: any) => {
         getProjectData(id);
     }, []);
 
-
     if (isLoading) {
         return (
             <div className='spin-block'>
@@ -24,6 +24,7 @@ const ProjectPage = ({ path, getProjectData, id, project, isLoading }: any) => {
             <div className='page project'>
                 <HeaderProject project={project}/>
                 <DragDropContexComponent project = {project}/>
+                <MessageContainer />
             </div>
         );
     }
